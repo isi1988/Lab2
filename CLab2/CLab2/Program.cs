41feca2df2eec1d168829accfa097f59c2317cb4
Nikita <Shishov.44@mail.ru>
2021-03-17 12:04:04 +0300

    new code

--- CLab2/CLab2/Program.cs
+++ CLab2/CLab2/Program.cs
@@ -7,7 +7,21 @@ namespace CLab2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+<<<<<<< HEAD
             Console.WriteLine("Hello Sviatoslav2");
+=======
+            Console.WriteLine("Hello Sviatoslav24");
+
+            Console.WriteLine("Hello Sviatoslav24");
+
+
+            Console.WriteLine("Hello Sviatoslav24");
+
+            Console.WriteLine("Hello Sviatoslav24");
+            Console.WriteLine("Hello Ilya");
+            Console.WriteLine("Hi Dave");
+            Console.WriteLine("Hello Nikita")
+>>>>>>> 607257e0cf5d9f81b22c96c54d2fa6a065a12ede
         }
     }
 }
